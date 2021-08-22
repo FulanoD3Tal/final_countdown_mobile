5d9c9c871f77af2507ad5891862a783dec5727e5
FulanoD3Tal <su.escala.es.biblica@gmail.com>
2021-08-22 10:53:13 -0500

    Inicial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Final Countdown
+
+Keep track of all your goals of countdown in a single place
---
+++ android/app/src/main/kotlin/com/example/final_coutdown/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.final_coutdown
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
