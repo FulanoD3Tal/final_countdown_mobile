a53160f78a08e67400d7eac381b390d4ed4d73e3
FulanoD3Tal <su.escala.es.biblica@gmail.com>
2023-06-19 10:13:21 -0600

    feat:add ump sdk for europe

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
-import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart'
+    hide
+        DebugGeography,
+        ConsentStatus,
+        ConsentRequestParameters,
+        ConsentDebugSettings;
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:user_messaging_platform/user_messaging_platform.dart';
 
 import 'package:final_coutdown/db_helpers/sqlite_helper.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -57,13 +63,21 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final db = CountdownProvider.instance;
+
+  // final _ump = UserMessagingPlatform.instance;
+  // TrackingAuthorizationStatus? _consentInformation;
+  // bool _tagAsUnderAgeOfConsent = false;
+  // bool _debugSettings = false;
+  // String? _testDeviceId;
+  // DebugGeography _debugGeography = DebugGeography.disabled;
+
   late List<Countdown> countdowns;
   bool isLoading = false;
 
   @override
   void initState() {
     super.initState();
-
+    updateConsent();
     refreshCountdowns();
   }
 
@@ -71,6 +85,25 @@ class _MyHomePageState extends State<MyHomePage> {
   void dispose() {
     db.close();
     super.dispose();
+  }
+
+  void updateConsent() async {
+    // Make sure to continue with the latest consent info.
+    var _ump = UserMessagingPlatform.instance;
+    var info = await _ump.requestConsentInfoUpdate(
+        // new ConsentRequestParameters(
+        //   debugSettings: new ConsentDebugSettings(
+        //     testDeviceIds: ['F4BD52507B2A84800D794A72477524A6'],
+        //     geography: DebugGeography.EEA,
+        //   ),
+        // ),
+        );
+
+    // Show the consent form if consent is required.
+    if (info.consentStatus == ConsentStatus.required) {
+      // `showConsentForm` returns the latest consent info, after the consent from has been closed.
+      info = await UserMessagingPlatform.instance.showConsentForm();
+    }
   }
 
   Future refreshCountdowns() async {
